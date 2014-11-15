70f2eada87d58383379a91c2f586f4d3b94219c8
ceifba2014 <ceifba2014@gmail.com>
2014-11-15 11:23:31 -0300

    TESTE
    15/11

--- php/sites/all/modules/authcache/modules/authcache_boost/README.txt
+++
@@ -1,6 +0,0 @@
-INSTALLATION
-============
-
-* Install and enable authcache, boost and authcache_boost modules.
-* Add the following line to your settings.php:
-  $conf['authcache_cache_handler'] = 'authcache_boost_cache_save';
--- php/sites/all/modules/authcache/modules/authcache_statistics/authcache_statistics.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-  $(function() {
-    if (Drupal.settings.authcacheStatistics && Drupal.settings.authcacheStatistics.log) {
-      $.AuthcacheAjax.cmd('statistics', Drupal.settings.authcacheStatistics);
-    }
-  });
-}(jQuery));
